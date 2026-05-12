eff30f2a497d0ae579c8b2005ed0f3e8ac876b4b
Dhiraj123-star <gdhiraj499@gmail.com>
2026-05-13 01:28:45 +0530

    feat: add live weather tool calling

--- app.py
+++ app.py
@@ -1,5 +1,7 @@
 import json
 import os
+import urllib.parse
+import urllib.request
 
 from dotenv import load_dotenv
 from anthropic import Anthropic
@@ -14,15 +16,49 @@ api_key = os.getenv("ANTHROPIC_API_KEY")
 client = Anthropic(api_key=api_key)
 
 # ----------------------
-# Local Tool function
+# Weather Function
 # ----------------------
 
-def get_weather(city:str):
-    return {
-        "city":city,
-        "temperature":"32°C",
-        "condition":"Sunny"
-    }
+def get_current_weather(city:str,unit:str="celsius") ->str:
+
+    try: 
+        # ----------------
+        # Geocoding API
+        # ----------------
+        geo_url = (
+            "https://geocoding-api.open-meteo.com/v1/search"
+            f"?name={urllib.parse.quote(city)}&count=1"
+        )
+
+        with urllib.request.urlopen(geo_url) as response:
+            geo_data= json.loads(response.read().decode())
+        
+        if "results" not in geo_data:
+            return f"City not found: {city}"
+
+        lat= geo_data["results"][0]["latitude"]
+        lon= geo_data["results"][0]["longitude"]
+
+        # ----------------
+        # Weather API
+        # ----------------
+
+        weather_url =  (
+            "https://api.open-meteo.com/v1/forecast"
+            f"?latitude={lat}&longitude={lon}&current=temperature_2m"
+        )
+        with urllib.request.urlopen(weather_url)as response:
+            weather_data = json.loads(response.read().decode())
+
+        temp = weather_data["current"]["temperature_2m"]
+
+        return f"Weather in {city}: {temp}°C"
+    
+    except Exception as ex:
+        return f"Error: {ex}"
+
+
+
 
 # -----------------
 # Tool Definition
@@ -72,7 +108,7 @@ for content in response.content:
         print(tool_input)
 
         if tool_name=="get_weather":
-            result = get_weather(tool_input["city"])
+            result = get_current_weather(tool_input["city"])
 
             # Send tool result back
             final_response= client.messages.create(
